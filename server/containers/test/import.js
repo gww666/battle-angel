14ff8618df144faf886fdba73285bdb130fc19a6
qixingzhi <qixingzhi@DR.COM>
2019-03-23 18:00:18 +0800

    qxz 列表接口

--- server/containers/test/import.js
+++ server/containers/test/import.js
@@ -1,1 +1,1 @@
-import header1 from "component/header/header1";import input3 from "component/input/input3";export default {components: {header1,input3,}}
+import input1 from "component/input/input1";import input2 from "component/input/input2";export default {components: {input1,input2,}}
--- server/ssr/api.js
+++ server/ssr/api.js
@@ -11,22 +11,26 @@ router.post("/compiler", compiler);
 
 // 列表的接口
 router.get('/getKitsList', async (ctx, next) => {
-    // let stream = fs.createReadStream(path.resolve(__dirname, '../public/static/json/kits.json'), 'utf8')
-    // let str = ''
-    // stream.on('data', chunk => {
-    //     str += chunk
-    // })
-    // stream.on('end', () => {
-    //     console.log(JSON.parse(str), typeof JSON.parse(str), 'end')
-    // })
     let data = []
-    try{
-        data = fs.readFileSync(path.resolve(__dirname, '../public/static/json/kits.json'), 'utf8')
-        data = JSON.parse(data)
-        ctx.body = new SucModel(data, '获取列表成功');
-    }catch(err) {
-        ctx.body = new ErrModel(data, '获取列表失败');
-    }
+    let kitClass = fs.readdirSync(path.resolve(__dirname, '../components'))
+    Array.isArray(kitClass) && kitClass.map(ele => {
+        let obj = {
+            type: ele,
+            title: ele,
+            list: []
+        }
+        let kitName = fs.readdirSync(path.resolve(__dirname, `../components/${ele}`))
+        Array.isArray(kitName) && kitName.map(item => {
+            item = item.split('.')[0]
+            obj.list.push({
+                name: item,
+                path: `component/${ele}/${item}`,
+                type: item
+            })
+        })
+        data.push(obj)
+    })
+    ctx.body = new SucModel(data, '获取列表成功');
     await next()
 })
 module.exports = router;
